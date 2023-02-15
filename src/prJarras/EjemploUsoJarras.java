410b919e19e5cc5ef8059b38c655ba898c4d3151
Cristark02 <mihaimita02@gmail.com>
2023-02-15 23:26:38 +0100

    Revert "Revert "Ejemplo terminado""
    This reverts commit a750e5491821c2056da678267dcce587fb0ba4ab.

---
+++ src/prJarras/EjemploUsoJarras.java
@@ -0,0 +1,18 @@
+package prJarras;
+
+public class EjemploUsoJarras
+{
+    public static void main(String[] args)
+    {
+
+    int n = 5;
+
+    Jarra jarraA = new Jarra(5);
+    System.out.println(jarraA);
+
+    Jarra jarraB = new Jarra(7);
+    System.out.println(jarraB);
+
+    }
+
+}
