8f9c47294f7395ad4c3ca5337893e6fbc5c02492
Cristark02 <mihaimita02@gmail.com>
2023-02-17 18:01:06 +0100

    Ejemplo Terminado

--- src/prJarras/EjemploUsoMesa.java
+++ src/prJarras/EjemploUsoMesa.java
@@ -1,4 +1,33 @@
 package prJarras;
 
-public class EjemploUsoMesa {
+public class EjemploUsoMesa
+{
+    public static void main (String[] args)
+    {
+        Mesa m = new Mesa(7, 5);
+
+        m.llena(2);
+        System.out.println(m);
+
+        m.llenaDesde(2);
+        System.out.println(m);
+
+        m.llena(2);
+        System.out.println(m);
+
+        m.llenaDesde(2);
+        System.out.println(m);
+
+        m.vacia(2);
+        System.out.println(m);
+
+        m.llenaDesde(2);
+        System.out.println(m);
+
+        m.llena(2);
+        System.out.println(m);
+
+        m.llenaDesde(2);
+        System.out.println(m);
+    }
 }
